80c14f6513585b9376b69c7a1b93b3d13ce57856
joeokivie <josephokivie@hotmail.com>
2024-01-12 14:50:33 -0500

    push code

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,7 +31,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  upTag.findOne({
+  Tag.findOne({
     where: {
       id: req.params.id,
     },
